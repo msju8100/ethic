f09f39e78d1e8730e2b7bcbd300d2238c21cfe58
msju8100 <msju8100@gmail.com>
2023-02-10 07:19:44 +0900

    v1

---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// gnb
+$('.main_menu > li').on('mouseover focusin', function(){
+    $('.sub_menu').removeClass('on');
+    $(this).children('ul').addClass('on');
+});
+$('.main_menu > li').on('mouseout', function(){
+    $(this).children('ul').removeClass('on');
+});
+$('.main_menu > li li:last-child a').on('focusout', function(){
+    $(this).parents('ul').removeClass('on');
+});
+
+// util
+$('.lang_default').on('click focusin', function () {
+    $(this).parent().toggleClass('active');
+});
+$('.lang_dropdown > li:last-child a').on('focusout', function () {
+    $(this).parent().parent().parent().removeClass('active');
+});
+
+// familysite
+$('.family_txt').on('click focusin', function () {
+    $(this).parent().toggleClass('active');
+});
+
+//맨 위로 이동
+$('.goto_top').click(function () {
+    $('html').animate({
+        scrollTop: 0
+    });
+});
+
+$(window).on('scroll', function(){
+    
+    const headerHeight = $('header').outerHeight();
+    const scrollTop = $(window).scrollTop();
+    const elTop = $('.main_visual').offset().top;
+
+    if(scrollTop > headerHeight){
+        $('header').addClass('fixed');
+    } else {
+        $('header').removeClass('fixed');
+    }
+
+    if(scrollTop >= elTop){
+        $('.btn_top').show(); 
+    } else {
+        $('.btn_top').hide(); 
+    }
+});
